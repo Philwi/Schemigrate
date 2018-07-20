f3a74e0d0fdaffcc0375a7de660290163434081a
Philwi <philoc@freenet.de>
2018-07-20 14:41:54 +0200

    multiservices

--- lib/schemigrate/schema_statements.rb
+++ lib/schemigrate/schema_statements.rb
@@ -1,6 +1,7 @@
 module Schemigrate
   module SchemaStatements
     def create_foreign_connection foreign_server
+      binding.pry
       database.create_fdw_extension foreign_server
       database.create_server_connection foreign_server
       database.create_user_mapping foreign_server
