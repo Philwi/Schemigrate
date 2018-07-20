9cbb3b5d44339a5d03b7be21b06d107575eedcf4
Philwi <philoc@freenet.de>
2018-07-20 14:36:10 +0200

    multiple services

--- lib/schemigrate/database.rb
+++ lib/schemigrate/database.rb
@@ -46,14 +46,14 @@ module Schemigrate
 				case server_config['dbsystem']
 				when 'MySQL'
 					execute <<-SQL
-						CREATE USER MAPPING FOR IF NOT EXISTS #{current_user}
+						CREATE USER MAPPING IF NOT EXISTS FOR#{current_user}
 						SERVER #{server_config['service']}
 						OPTIONS (username '#{server_config['user']}',
 										password '#{server_config['password']}')
 					SQL
 				when 'PostgreSQL'
 					execute <<-SQL
-						CREATE USER MAPPING FOR IF NOT EXISTS #{current_user}
+						CREATE USER MAPPING IF NOT EXISTS FOR #{current_user}
 						SERVER #{server_config['service']}
 						OPTIONS (user '#{server_config['user']}',
 										password '#{server_config['password']}')
