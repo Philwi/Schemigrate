5151ca6c980b03baab7ee2d67eeea15e5807a5f2
Philwi <philoc@freenet.de>
2018-07-20 13:42:33 +0200

    forgot the args...

--- lib/schemigrate/schema_statements.rb
+++ lib/schemigrate/schema_statements.rb
@@ -4,8 +4,8 @@ module Schemigrate
       database.create_fdw_extension foreign_server
       database.create_server_connection foreign_server
       database.create_user_mapping foreign_server
-      database.create_schema
-      database.import_foreign_schema
+      database.create_schema foreign_server
+      database.import_foreign_schema foreign_server
     end
   private
     def database
