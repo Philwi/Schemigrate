fd74df84234e8d79c55a77aea35a306c477099b3
Philwi <philoc@freenet.de>
2018-07-20 17:31:03 +0200

    multiservices

--- lib/schemigrate/database.rb
+++ lib/schemigrate/database.rb
@@ -1,99 +1,110 @@
 module Schemigrate
   class Database
     def create_fdw_extension server
-      binding.pry
       database_configuration[Rails.env].each_with_index do |s, index|
         server_config = database_configuration[Rails.env].values[index]
-        case server_config['dbsystem']
-        when 'MySQL'
-          enable_extension :mysql_fdw
-        when 'PostgreSQL'
-          enable_extension :postgres_fdw
-        else
-          puts 'Wrong nor implemented database-system.'
+        begin
+          case server_config["dbsystem"]
+          when "MySQL"
+            enable_extension :mysql_fdw
+          when "PostgreSQL"
+            enable_extension :postgres_fdw
+          else
+            puts 'Wrong nor implemented database-system.'
+          end
+        rescue StandardError => e
+          puts "Failure: #{e}"
         end
       end
     end
 
     def create_server_connection server
-      binding.pry
       database_configuration[Rails.env].each_with_index do |s, index|
         server_config = database_configuration[Rails.env].values[index]
-				case server_config['dbsystem']
-				when 'MySQL'
-					execute <<-SQL
-						CREATE SERVER IF NOT EXISTS #{server_config['service']}
-						FOREIGN DATA WRAPPER mysql_fdw
-						OPTIONS (host '#{server_config['host']}',
-										port '#{server_config['port']}',
-										dbname '#{server_config['dbname']}')
-					SQL
-				when 'PostgreSQL'
-					execute <<-SQL
-						CREATE SERVER IF NOT EXISTS #{server_config['service']}
-						FOREIGN DATA WRAPPER postgres_fdw
-						OPTIONS (host '#{server_config['host']}',
-										port '#{server_config['port']}',
-										dbname '#{server_config['dbname']}')
-					SQL
-				else
-					puts 'Wrong nor implemented database-system.'
-				end
+        begin
+          case server_config['dbsystem']
+          when "MySQL"
+            execute <<-SQL
+              CREATE SERVER IF NOT EXISTS #{server_config['service']}
+              FOREIGN DATA WRAPPER mysql_fdw
+              OPTIONS (host '#{server_config['host']}',
+                      port '#{server_config['port']}',
+                      dbname '#{server_config['dbname']}')
+            SQL
+          when "PostgreSQL"
+            execute <<-SQL
+              CREATE SERVER IF NOT EXISTS #{server_config['service']}
+              FOREIGN DATA WRAPPER postgres_fdw
+              OPTIONS (host "#{server_config['host']}",
+                      port "#{server_config['port']}",
+                      dbname "#{server_config['dbname']}")
+            SQL
+          else
+            puts 'Wrong nor implemented database-system.'
+          end
+        rescue StandardError => e
+          puts "Failure: #{e}"
+        end
 			end
     end
 
 		def create_user_mapping server
-      binding.pry
 			database_configuration[Rails.env].each_with_index do |s, index|
 				server_config = database_configuration[Rails.env].values[index]
-				case server_config['dbsystem']
-				when 'MySQL'
-					execute <<-SQL
-						CREATE USER MAPPING IF NOT EXISTS FOR#{current_user}
-						SERVER #{server_config['service']}
-						OPTIONS (username '#{server_config['user']}',
-										password '#{server_config['password']}')
-					SQL
-				when 'PostgreSQL'
-					execute <<-SQL
-						CREATE USER MAPPING IF NOT EXISTS FOR #{current_user}
-						SERVER #{server_config['service']}
-						OPTIONS (user '#{server_config['user']}',
-										password '#{server_config['password']}')
-					SQL
-				else
-					puts 'Wrong nor implemented database-system.'
-				end
+        begin
+          case server_config['dbsystem']
+          when "MySQL"
+            execute <<-SQL
+              CREATE USER MAPPING IF NOT EXISTS FOR#{current_user}
+              SERVER #{server_config['service']}
+              OPTIONS (username "#{server_config['user']}",
+                      password "#{server_config['password']}")
+            SQL
+          when "PostgreSQL"
+            execute <<-SQL
+              CREATE USER MAPPING IF NOT EXISTS FOR #{current_user}
+              SERVER #{server_config['service']}
+              OPTIONS (user "#{server_config['user']}",
+                      password "#{server_config['password']}")
+            SQL
+          else
+            puts 'Wrong nor implemented database-system.'
+          end
+        rescue StandardError => e
+          puts "Failure: #{e}"
+        end
 			end
 		end
 
 		def create_schema server
-      binding.pry
 			database_configuration[Rails.env].each_with_index do |s, index|
 				server_config = database_configuration[Rails.env].values[index]
-				execute <<-SQL
-					CREATE SCHEMA IF NOT EXISTS #{server_config['service']}
-				SQL
+        begin
+          execute <<-SQL
+            CREATE SCHEMA IF NOT EXISTS #{server_config['service']}
+          SQL
+        rescue StandardError => e
+          puts "Failure #{e}"
+        end
 			end
 		end
 
 		def import_foreign_schema server
-      binding.pry
 			database_configuration[Rails.env].each_with_index do |s, index|
 				server_config = database_configuration[Rails.env].values[index]
-				if server_config['expect'].nil?
-					puts "noch nicht implementiert"
-				else
-          begin
+        begin
+          if server_config['expect'].nil?
+            puts "noch nicht implementiert"
+          else
             execute <<-SQL
               IMPORT FOREIGN SCHEMA #{server_config['schema']}
               FROM SERVER #{server_config['service']}
               INTO #{server_config['service']}
             SQL
-          rescue StandardError => e
-            puts "Error: #{e}"
           end
-				end
+        rescue StandardError => e
+          puts "Failure: #{e}"
+        end
 			end
 		end
 
